951778507c476cc6e8b5f91e8a1f3cd2b485ec3e
davidshyn1 <59733867+davidshyn1@users.noreply.github.com>
2020-07-06 00:46:47 +0900

    Update test.py

--- smoothness_measure/test.py
+++ smoothness_measure/test.py
@@ -1,19 +1,19 @@
 import GKFN
-import ft
+import ft_monthly
 
-data = ft.readData('2000-01-03', '2020-03-13')
+data = ft_monthly.readData('1960-01-01', '2020-06-01')
 
 # 선택된 E, tau 값을 이용하여 데이터를 재구성합니다.
-E = 5
-tau = 3
+E = 4
+tau = 2
 P = 1 # P는 몇일 뒤 값을 예측할 지 설정 해주는 parameter입니다. 예를 들어 하루 뒤 값을 예측하는 것이면 P = 1
 
 # train set 및 test set을 구성합니다.
-dataX, dataY  = ft.extracting(tau, E, P, data)
-trX = dataX[:-1580]
-teX = dataX[-1580:]
-trY = dataY[:-1580]
-teY = dataY[-1580:]
+dataX, dataY  = ft_monthly.extracting(tau, E, P, data)
+trX = dataX[:-142]
+teX = dataX[-142:]
+trY = dataY[:-142]
+teY = dataY[-142:]
 
 # parameter를 설정하고 학습을 시킵니다.
 alpha = 0.5
