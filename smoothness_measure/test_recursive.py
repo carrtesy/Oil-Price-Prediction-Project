6c1b7da7ea2c8d127147579aa6200532aee2bf06
davidshyn1 <59733867+davidshyn1@users.noreply.github.com>
2020-07-15 08:33:29 +0900

    Create test_recursive.py

---
+++ smoothness_measure/test_recursive.py
@@ -0,0 +1,58 @@
+import GKFN_recursive
+import ft
+
+#mode = "daily"
+#mode = "weekly_origin"
+mode = "weekly_tau1"
+#mode = "monthly"
+
+dailyfile = open('./daily/wti.csv', 'r')
+weeklyfile = open('./weekly/wti_week.csv', 'r')
+monthlyfile = open('./monthly/wti_month.csv', 'r')
+
+if(mode == "daily"): # Daily
+    print("===DAILY DATASET===")
+    data = ft.readData(dailyfile, '2000-01-03', '2020-03-13')
+    E = 5
+    tau = 3
+elif(mode == "weekly_origin"): # Weekly_original
+    print("===WEEKLY DATASET===")
+    data = ft.readData(weeklyfile, '1986-01-03', '2020-06-26')
+    E = 4
+    tau = 5
+elif(mode == "weekly_tau1"): # Weekly_tau1
+    print("===WEEKLY DATASET===")
+    data = ft.readData(weeklyfile, '1986-01-03', '2020-06-26')
+    E = 6
+    tau = 1
+elif(mode == "monthly"): # Monthly
+    print("===MONTHLY DATASET===")
+    data = ft.readData(monthlyfile, '1960-01-01', '2020-06-01')
+    E = 4
+    tau = 2
+
+
+P = 1 # P days/weeks/months after
+pred = None
+for t in range(4):
+    dataX, dataY = ft.extracting_recursive(tau, E, P, t, pred, data)
+    test_ratio = 0.3
+    test_size = int(len(data) * test_ratio)
+    print("size of dataset:", len(data))
+    print("size of test dataset:", test_size)
+
+    # Train
+    trX = dataX[:-test_size]
+    trY = dataY[:-test_size]
+
+    # Test
+    teX = dataX[-test_size:]
+    teY = dataY[-test_size:]
+
+    # parameter
+    alpha = 0.5
+    loop = 5
+    Kernel_Num = 31
+
+    pred = GKFN_recursive.GKFN(trX, trY, teX, teY, alpha, loop, Kernel_Num,t)
+
