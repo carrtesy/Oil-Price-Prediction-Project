59e3c6924cbe36a2457e2dfe353e2da4a18bb2f1
Dongmin Kim <dongmin.kim.0220@gmail.com>
2020-10-06 18:59:07 +0900

    weekly rolling forecast(r2 98.07)

--- models/test_rolling_forecast_new.py
+++ models/test_rolling_forecast_new.py
@@ -8,10 +8,10 @@ from dateutil.rrule import FR
 import data
 '''
 #mode = "daily"
-#mode = "weekly_origin"
+mode = "weekly_origin"
 #mode = "weekly_tau1"
 #mode = "weekly_for_monthly"
-mode = "monthly"
+#mode = "monthly"
 #mode = "weekly_data+"
 #mode = "monthly_data+"
 
@@ -114,7 +114,7 @@ evaluate
 # plot formatters
 formatter = mdates.DateFormatter("%Y-%m-%d") # date format for plotting
 #locater = mdates.DayLocator(interval = 180) # for daily data
-#locater = mdates.WeekdayLocator(byweekday = FR, interval = 26 * 4) # weekly
-locater = mdates.MonthLocator(bymonthday = 1, interval = 6) # monthly
+locater = mdates.WeekdayLocator(byweekday = FR, interval = 26 * 4) # weekly
+#locater = mdates.MonthLocator(bymonthday = 1, interval = 6) # monthly
 
 GKFN.plot_prediction(teY, teYdate, Yest, num_kernels, epoch, formatter, locater)
